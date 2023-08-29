4d09762936051c81f86d87e10142e439c58bac3a
kmmasden <kaitmasdes@gmail.com>
2023-08-29 14:23:55 -0400

    pushing some more worked code

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Kait's Godot Repo 
+# Kait's Code Repo 
 Inside:
+- java: Will contain java code I have written 
+- gdscript: contains gdscript code and godot projects  
   - tutorial: code and projects that I've created following tutorials
   - mine: my own separate projects that I have created independently to reinforce, better learn what I've learned in the tutorials
--- java/leetcode/solutions/P14_Lcf.java
+++ java/leetcode/solutions/P14_Lcf.java
@@ -1,36 +1,9 @@
+package solutions;
+
 import java.util.Arrays;
 
-public class Solutions {
+public class P14_Lcf {
     
-
-
-    // 1. https://leetcode.com/problems/two-sum
-    public int[] twoSum(int[] nums, int target) {
-            int[] oArr = {0, 1};
-    
-            //we know nums.length will be >= 2 from the constraints; so return default answer array if only length of 2
-            if (nums.length == 2) {
-                return oArr; 
-            }
-    
-            //otherwise, loop through w/nested for loop and 2 pointers to check for target sum
-            int tSum = 0; 
-            for (int i = 0; i<nums.length; i++) {
-                for (int j = 1; j < nums.length; j++) {
-                    if (i != j) {
-                        tSum = nums[i] + nums[j]; 
-                        if (tSum == target) {
-                            oArr[0] = i; 
-                            oArr[1] = j; 
-                            break;
-                        }
-                    }
-                }
-            }
-            return oArr; 
-        }
-
-
     // 14. longest common prefix
     public String longestCommonPrefix (String[] strs) {
             String answer = "";
@@ -60,7 +33,7 @@ public class Solutions {
             }
 
             answer += check.substring(0, 1); 
-            
+
             for (int i = 1; i < shortestLength; i++) {
                 char checkChar = check.charAt(i); 
                 boolean match = true; 
@@ -80,8 +53,5 @@ public class Solutions {
             }
         return answer;
     }
-
-
-
 
 }
