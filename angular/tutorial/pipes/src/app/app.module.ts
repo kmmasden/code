99bbf9e2901ac969d8f916729fa308bd02e2ae24
kmmasden <kaitmasdes@gmail.com>
2023-09-02 13:57:31 -0400

    completed 'pipes project' using different built in pipes and making a custom one'

--- angular/tutorial/pipes/src/app/app.module.ts
+++ angular/tutorial/pipes/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DistancePipe } from './distance.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DistancePipe
   ],
   imports: [
     BrowserModule
