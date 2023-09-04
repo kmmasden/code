99603f2cdb4d2db4798c314c76bcd15313bbbf28
kmmasden <kaitmasdes@gmail.com>
2023-09-04 12:52:25 -0400

    NOW through section 9

--- angular/tutorial/sGriderUdemy/showcase/src/app/collections/collections-home/collections-home.component.ts
+++ angular/tutorial/sGriderUdemy/showcase/src/app/collections/collections-home/collections-home.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent {
+  data = [
+    {name: 'Geralt', age: 100, job: 'Witcher', employed: true},
+    {name: 'Yennefer', age: 90, job: 'Sorceress', employed: true},
+    {name: 'Triss', age: 40, job: 'Magic', employed: true},
+    {name: 'Jaskier', age: 40, job: 'Bard', employed: false},
+    {name: 'Ciri', age: 21, job: 'Princess', employed: false}
+  ];
 
+  headers = [
+    { key: 'name', label: 'Name'},
+    { key: 'age', label: 'Age'},
+    { key: 'job', label: 'Job'},
+    { key: 'employed', label: 'Employed'}
+  ]; 
 }
