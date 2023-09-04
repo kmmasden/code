687e7c2032d960aff794ec87c034c248443133f8
kmmasden <kaitmasdes@gmail.com>
2023-09-04 12:54:56 -0400

    beginning S10, advanced component routing- we will create a tab component that will changed based on the address the user is at'

--- angular/tutorial/sGriderUdemy/showcase/src/app/collections/collections.module.ts
+++ angular/tutorial/sGriderUdemy/showcase/src/app/collections/collections.module.ts
@@ -5,12 +5,18 @@ import { SharedModule } from '../shared/shared.module';
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 import { TableComponent } from './table/table.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CompanyComponent } from './company/company.component';
+import { PartnersComponent } from './partners/partners.component';
 
 
 @NgModule({
   declarations: [
     CollectionsHomeComponent,
-    TableComponent
+    TableComponent,
+    BiographyComponent,
+    CompanyComponent,
+    PartnersComponent
   ],
   imports: [
     CommonModule,
